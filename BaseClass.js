aedae619c100bd3484bcd4c971c4b99985a1b13e
aditya <geethaaliyangal@gmail.com>
2020-06-29 11:01:32 +0530

    messsge

--- BaseClass.js
+++ BaseClass.js
@@ -1,9 +1,10 @@
 class BaseClass{
     constructor(x, y, width, height, angle) {
         var options = {
+          isStatic:false,
             'restitution':0.8,
             'friction':1.0,
-            'density':0.5
+            'density':0.04
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
